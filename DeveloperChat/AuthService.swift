e7840dd8b83ae16ea55f0c43151e58b8ed8fee36
Joe Kim <josephkim0224@gmail.com>
2017-02-06 10:49:53 -0500

    Login error handling complete

--- DeveloperChat/AuthService.swift
+++ DeveloperChat/AuthService.swift
@@ -9,6 +9,8 @@
 import Foundation
 import FirebaseAuth
 
+typealias Completion = (_ errMsg: String?, _ data: AnyObject?) -> Void
+
 class AuthService {
     private static let _instance = AuthService()
     
@@ -16,22 +18,25 @@ class AuthService {
         return _instance
     }
     
-    func login(email: String, password: String) {
+    func login(email: String, password: String, onComplete: Completion?) {
         FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
             if error != nil  {
                 if let errorCode = FIRAuthErrorCode(rawValue: error!._code) {
                     if errorCode == .errorCodeUserNotFound {
                         FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
                             if error != nil {
-                                // Show error to user
+                                // Account already exists
+                                self.handleFirebaseError(error: error! as NSError, onComplete: onComplete)
                             } else {
-                                if user.uid != nil {
+                                if user?.uid != nil {
                                     // Sign in
                                     FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+                                        // Signin Failure
                                         if error != nil {
-                                            //Show error to user
+                                            self.handleFirebaseError(error: error! as NSError, onComplete: onComplete)
                                         } else {
-                                            // we have successfully logged in
+                                            // Login successful - new account created
+                                            onComplete?(nil, user)
                                         }
                                     })
                                 }
@@ -40,10 +45,37 @@ class AuthService {
                     }
                 } else {
                     // Handle all other errors
+                    self.handleFirebaseError(error: error! as NSError, onComplete: onComplete)
                 }
             } else {
-                // Successfully Logged in
+                // Successfully Logged in - without creating an account
+                onComplete?(nil, user)
+                
             }
         })
     }
+    
+    func handleFirebaseError(error: NSError, onComplete: Completion?) {
+        print(error.debugDescription)
+        if let errorCode = FIRAuthErrorCode(rawValue: error._code) {
+            switch(errorCode) {
+            case .errorCodeInvalidEmail:
+                onComplete?("Invalid Email Address", nil)
+                break
+            case .errorCodeWrongPassword:
+                onComplete?("Invalid Password", nil)
+                break
+            case .errorCodeWeakPassword:
+                onComplete?("Password must be at least 6 characters", nil)
+                break
+            case .errorCodeEmailAlreadyInUse, .errorCodeAccountExistsWithDifferentCredential:
+                onComplete?("Could not create account. Email already in use", nil)
+                break
+            default:
+                onComplete?("There was a problem authenticating. Try again.", nil)
+            }
+        }
+    }
+    
+    
 }
